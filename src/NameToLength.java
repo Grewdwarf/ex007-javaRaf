1c40ef4e716ff5cd9df6276b249578ff8a3b2f5c
Love Gruvberg <Grooveberg@Loves-MacBook-Pro.local>
2021-03-13 00:22:39 +0100

    Hello?

---
+++ src/NameToLength.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NameToLength 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter your name to find out its length: ");
+		Scanner yourName = new Scanner(System.in);
+		String name = yourName.nextLine();
+		char [] ch = name.toCharArray();
+		int nameLength = 0;
+
+		for (int i = 0; i < ch.length; i++) 
+		{
+			if (ch[i]!=' ') 
+			{
+				nameLength++;
+			}
+		}
+		System.out.println(nameLength++);
+	}
+}
